9cc843c2aa1cb2968665c6d760c9a25f55c584f9
Engenharia 02 <engenharia02@industria.local>
2025-01-08 15:15:51 -0300

    Operação de soma

--- calculadora.py
+++ calculadora.py
@@ -1,13 +1,20 @@
-def verificarEntradas():
-	resultado=0
+def somar():
 	num1= input("Digite o primeiro numero: ")
 	num2= input("Digite o segundo numero: ")
+	soma=verificarEntradas(num1,num2)
+	if isinstance(soma,int):
+		return soma
+	else:
+		return 0	
+
+def verificarEntradas(num1,num2):
 	try:
-		int(num1)
-		int(num2)	
+		primeiroNumero=int(num1)
+		segundoNumero=int(num2)
+		return primeiroNumero+segundoNumero
 	except Exception as e:
 		print("Os dois valores devem ser numericos")
- 
+	
 
 print("------Calculadora------\n")
 
@@ -23,7 +30,8 @@ while (opcao != -1):
 	print(f'Vc selecionou a opção {opcao}')
 	match (opcao):
 		case 1:
-			result= verificarEntradas()
+			result= somar()
+			print(f'Resultado da soma: {result}')
 		case 2:
 			break
 		case 3:
