3edd4e04743009b257d316349b64b082da7164e4
Engenharia 02 <engenharia02@industria.local>
2025-01-08 09:24:34 -0300

    Inicio do projeto calculadora simples

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+print("------Calculadora------\n")
+
+print("Digite o numero da operacao desejada ou -1 para sair:\n")
+print("1-Soma.")
+print("2-Subtração.")
+print("3-Divisão. ")
+print("4-Multiplicacao. \n")
+
+opcao=0
+while (opcao != -1):
+	opcao= int(input("Selecione a opção desejada: "))
+	print(f'Vc selecionou a opção {opcao}')
+	match (opcao):
+		case 1:
+			break
+		case 2:
+			break
+		case 3:
+			break
+		case 4:
+			break
+		case _:
+			break
+			 				
+			
+	
+
+
